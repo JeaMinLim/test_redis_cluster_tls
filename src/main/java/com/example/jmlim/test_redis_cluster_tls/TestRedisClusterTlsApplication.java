090bba345e83b99765f6ce5e0e0d115b640966dd
JeaMinLim <gam1532@gmail.com>
2024-08-31 10:52:47 +0900

    2024-08-31 First commit for redis cluster tls test code using SpringBoot 2

---
+++ src/main/java/com/example/jmlim/test_redis_cluster_tls/TestRedisClusterTlsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmlim.test_redis_cluster_tls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRedisClusterTlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRedisClusterTlsApplication.class, args);
+	}
+
+}
